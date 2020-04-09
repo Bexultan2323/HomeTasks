e64576ef21374fc2dcd1cde62c5b4a77b08e0aea
Bexultan2323 <57893439+Bexultan2323@users.noreply.github.com>
2020-04-09 12:58:18 +0600

    Employee

---
+++ employee.java
@@ -0,0 +1,53 @@
+/*package com.company;
+
+public class Employee {
+    public String Fname;
+    public String Lname;
+    public double MonthSalary;
+
+    public Employee(String employee_fname,String employee_lname,double employee_monthSalary){
+        Fname=employee_fname;
+        Lname=employee_lname;
+        MonthSalary=employee_monthSalary;
+    }
+    public String getFname() {
+        return Fname;
+    }
+    public void setFname(String employee_fname){
+        Fname=employee_fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+    public void setLname(String employee_lname){
+        Lname=employee_lname;
+    }
+
+
+    public double getMonthSalary() {
+        return MonthSalary;
+    }
+    public void setMonthSalary(double employee_monthSalary) {
+        if(MonthSalary>0) {
+            MonthSalary = employee_monthSalary;
+        }
+    }
+}*/
+/*package com.company;
+
+public class EmployeeTest {
+    public static void Main(String[] args) {
+        Employee employee1 = new Employee("Bex", "Zhex", 600000);
+        Employee employee2 = new Employee("Aaa", "Bbb", 500000);
+        System.out.println(employee1.getFname()+"year salary: "+12*employee1.getMonthSalary());
+        System.out.println(employee2.getFname()+"year salary: "+12*employee2.getMonthSalary());
+
+        double increasedSalary1=employee1.getMonthSalary()*1.1;
+        double increasedSalary2=employee2.getMonthSalary()*1.1;
+        employee1.setMonthSalary(increasedSalary1);
+        employee2.setMonthSalary(increasedSalary2);
+        System.out.println(employee1.getFname()+"year salary: "+12*employee1.getMonthSalary());
+        System.out.println(employee2.getFname()+"year salary: "+12*employee2.getMonthSalary());
+    }
+}*/
