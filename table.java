1dcdb936728a1f36e9b0497887491ca0bcdd6c7c
Bexultan2323 <57893439+Bexultan2323@users.noreply.github.com>
2020-04-09 13:13:51 +0600

    Table

---
+++ table.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+   Scanner circle=new Scanner(System.in);
+   System.out.println("number square cube");
+   for(int i=0;i<=10;i++){
+              System.out.print(i+" ");
+              System.out.print(i * i+" ");
+              System.out.print(i * i * i+" ");
+              System.out.println();
+          }
+    }
+}
