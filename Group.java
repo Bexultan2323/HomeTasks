be86005f1121bd9b4b4215c92c35cb07dc8cc657
Bexultan2323 <57893439+Bexultan2323@users.noreply.github.com>
2020-09-14 22:22:13 +0600

    Spring boot task1
    Spring boot task1

--- Group.java
+++ Group.java
@@ -1,12 +1,18 @@
-public class Group {
-    int id;
-    String name;
+package kz.aitu.advancedJava.model;
 
-    @Override
-    public String toString(){
-        return "Group{"
-                + "id='" + id + '\''
-                + ", name=" + name
-                + '}';
-    }
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "groups")
+public class Group {
+    @Id
+    private long id;
+    private String name;
 }
--- Student.java
+++ Student.java
@@ -1,16 +1,27 @@
-public class Student {
-    int id;
-    int groupid;
-    String name;
-    String phone;
+package kz.aitu.advancedJava.model;
 
-    @Override
-    public String toString(){
-        return "Student{"
-                + "id='" + id + '\''
-                + "groupid='" + groupid + '\''
-                + ", name=" + name
-                + ", phone=" + phone
-                + '}';
-    }
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    private long id;
+
+    private String name;
+
+    private String telephone;
+
+    private long groupId;
+
 }
